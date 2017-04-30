cf086054bd66e2d87b040490a78c23595d307c19
Benoit Pierre <benoit.pierre@gmail.com>
2017-05-01 00:09:23 +0200

    oslayer/keyboardcontrol: rework classes declaration
    Sub-classing the platform KeyboardCapture/KeyboardEmulation class
    as keyboardcontrol.KeyboardCapture can lead to non-obvious issues,
    e.g. because KeyboardCapture in the context of machine.keyboard
    is not the same class as KeyboardCapture in the context of
    oslayer.xkeyboardcontrol, making using class attributes more
    difficult.
    
    Also move the keys layout to the keyboard machine where it belongs.

--- plover/oslayer/winkeyboardcontrol.py
+++ plover/oslayer/winkeyboardcontrol.py
@@ -25,6 +25,7 @@ import winreg
 from ctypes import windll, wintypes
 
 from plover.key_combo import parse_key_combo
+from plover.oslayer.keyboardcontrol_base import KeyboardCaptureBase, KeyboardEmulationBase
 from plover.oslayer.winkeyboardlayout import KeyboardLayout
 from plover import log
 from plover.misc import to_surrogate_pair
@@ -400,14 +401,12 @@ class KeyboardCaptureProcess(multiprocessing.Process):
         return self._queue.get()
 
 
-class KeyboardCapture(threading.Thread):
+class KeyboardCapture(threading.Thread, KeyboardCaptureBase):
     """Listen to all keyboard events."""
 
     def __init__(self):
         super().__init__()
         self._suppressed_keys = set()
-        self.key_down = lambda key: None
-        self.key_up = lambda key: None
         self._proc = KeyboardCaptureProcess()
         self._finished = threading.Event()
 
@@ -437,7 +436,7 @@ class KeyboardCapture(threading.Thread):
         self._proc.suppress_keyboard(self._suppressed_keys)
 
 
-class KeyboardEmulation:
+class KeyboardEmulation(KeyboardEmulationBase):
 
     def __init__(self):
         self.keyboard_layout = KeyboardLayout()
